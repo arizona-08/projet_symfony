97db70fbb1f5ba3268342204947c01d682208a5a
Jonathan Assi <assijonathan2@gmail.com>
2024-12-10 00:03:38 +0100

    Register and login functional. I have to find an other way to make forms consistant and not use another method for each one.

--- migrations/Version20241210200403.php
+++ migrations/Version20241210200403.php
@@ -24,6 +24,7 @@ final class Version20241210200403 extends AbstractMigration
         $this->addSql('COMMENT ON COLUMN location.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN location.updated_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE "user" (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, email_verified_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, password VARCHAR(255) NOT NULL, remember_token VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE "user" ADD roles JSON NOT NULL');
         $this->addSql('COMMENT ON COLUMN "user".email_verified_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN "user".created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE vehicle (id SERIAL NOT NULL, location_id INT DEFAULT NULL, model VARCHAR(255) NOT NULL, marque VARCHAR(255) NOT NULL, last_maintenance DATE NOT NULL, nb_kilometrage INT NOT NULL, nb_serie VARCHAR(255) NOT NULL, price_per_day NUMERIC(10, 2) NOT NULL, PRIMARY KEY(id))');
---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+use function Symfony\Component\Clock\now;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            // encode the plain password
+            $name = $form->get('name')->getData();
+            $user->setName($name);
+            $user->setRoles(['ROLE_USER']);
+            $user->setEmailVerifiedAt(now());
+            $user->setCreatedAt(now());
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+}
