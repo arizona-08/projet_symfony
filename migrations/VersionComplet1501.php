fdaded0752dbc1a5cec8b5fd556a93610ee9efe6
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2025-01-15 08:36:11 +0100

    fix
    Il faut degager tt les mig psk sa fait chier , quand vous proposer des nouveaux ficheir de migraitons le pb c'est que vous proposer des ficheir de migg en focntion de voitre bdd qui nest pas toujours a jour donc sa creer de la redondance , JE PROPOSE UN FICHIER UPDATE COMPLET

--- migrations/VersionComplet1501.php
+++ migrations/VersionComplet1501.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250112191413 extends AbstractMigration
+final class Version20250115073143 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,7 @@ final class Version20250112191413 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE accessory (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE agency (id SERIAL NOT NULL, user_id INT NOT NULL, label VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, zip_code INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_70C0C6E6A76ED395 ON agency (user_id)');
         $this->addSql('CREATE TABLE car (id INT NOT NULL, four_wheel BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
@@ -27,7 +28,14 @@ final class Version20250112191413 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_D48A2F7C19EB6921 ON config (client_id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_D48A2F7C545317D1 ON config (vehicle_id)');
         $this->addSql('CREATE INDEX IDX_D48A2F7C3A8E60EF ON config (kit_id)');
-        $this->addSql('CREATE TABLE kit (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, accessory JSON NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE feedback (id SERIAL NOT NULL, location_id INT NOT NULL, client_id INT DEFAULT NULL, rating INT NOT NULL, comment TEXT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_D229445864D218E ON feedback (location_id)');
+        $this->addSql('CREATE INDEX IDX_D229445819EB6921 ON feedback (client_id)');
+        $this->addSql('COMMENT ON COLUMN feedback.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE kit (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE kit_accessory (kit_id INT NOT NULL, accessory_id INT NOT NULL, PRIMARY KEY(kit_id, accessory_id))');
+        $this->addSql('CREATE INDEX IDX_12E9B8E03A8E60EF ON kit_accessory (kit_id)');
+        $this->addSql('CREATE INDEX IDX_12E9B8E027E8CC78 ON kit_accessory (accessory_id)');
         $this->addSql('CREATE TABLE location (id SERIAL NOT NULL, user_id INT NOT NULL, config_id INT DEFAULT NULL, start_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, end_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, vip BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_5E9E89CBA76ED395 ON location (user_id)');
         $this->addSql('CREATE INDEX IDX_5E9E89CB24DB0683 ON location (config_id)');
@@ -49,8 +57,12 @@ final class Version20250112191413 extends AbstractMigration
         $this->addSql('ALTER TABLE config ADD CONSTRAINT FK_D48A2F7C19EB6921 FOREIGN KEY (client_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE config ADD CONSTRAINT FK_D48A2F7C545317D1 FOREIGN KEY (vehicle_id) REFERENCES vehicle (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE config ADD CONSTRAINT FK_D48A2F7C3A8E60EF FOREIGN KEY (kit_id) REFERENCES kit (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE feedback ADD CONSTRAINT FK_D229445864D218E FOREIGN KEY (location_id) REFERENCES location (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE feedback ADD CONSTRAINT FK_D229445819EB6921 FOREIGN KEY (client_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE kit_accessory ADD CONSTRAINT FK_12E9B8E03A8E60EF FOREIGN KEY (kit_id) REFERENCES kit (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE kit_accessory ADD CONSTRAINT FK_12E9B8E027E8CC78 FOREIGN KEY (accessory_id) REFERENCES accessory (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CBA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CB24DB0683 FOREIGN KEY (config_id) REFERENCES config (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CB24DB0683 FOREIGN KEY (config_id) REFERENCES config (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE motorcycle ADD CONSTRAINT FK_21E380E1BF396750 FOREIGN KEY (id) REFERENCES vehicle (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE vehicle ADD CONSTRAINT FK_1B80E48664D218E FOREIGN KEY (location_id) REFERENCES location (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE vehicle ADD CONSTRAINT FK_1B80E486CDEADB2A FOREIGN KEY (agency_id) REFERENCES agency (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -66,16 +78,23 @@ final class Version20250112191413 extends AbstractMigration
         $this->addSql('ALTER TABLE config DROP CONSTRAINT FK_D48A2F7C19EB6921');
         $this->addSql('ALTER TABLE config DROP CONSTRAINT FK_D48A2F7C545317D1');
         $this->addSql('ALTER TABLE config DROP CONSTRAINT FK_D48A2F7C3A8E60EF');
+        $this->addSql('ALTER TABLE feedback DROP CONSTRAINT FK_D229445864D218E');
+        $this->addSql('ALTER TABLE feedback DROP CONSTRAINT FK_D229445819EB6921');
+        $this->addSql('ALTER TABLE kit_accessory DROP CONSTRAINT FK_12E9B8E03A8E60EF');
+        $this->addSql('ALTER TABLE kit_accessory DROP CONSTRAINT FK_12E9B8E027E8CC78');
         $this->addSql('ALTER TABLE location DROP CONSTRAINT FK_5E9E89CBA76ED395');
         $this->addSql('ALTER TABLE location DROP CONSTRAINT FK_5E9E89CB24DB0683');
         $this->addSql('ALTER TABLE motorcycle DROP CONSTRAINT FK_21E380E1BF396750');
         $this->addSql('ALTER TABLE vehicle DROP CONSTRAINT FK_1B80E48664D218E');
         $this->addSql('ALTER TABLE vehicle DROP CONSTRAINT FK_1B80E486CDEADB2A');
         $this->addSql('ALTER TABLE vehicle DROP CONSTRAINT FK_1B80E4862ADD6D8C');
+        $this->addSql('DROP TABLE accessory');
         $this->addSql('DROP TABLE agency');
         $this->addSql('DROP TABLE car');
         $this->addSql('DROP TABLE config');
+        $this->addSql('DROP TABLE feedback');
         $this->addSql('DROP TABLE kit');
+        $this->addSql('DROP TABLE kit_accessory');
         $this->addSql('DROP TABLE location');
         $this->addSql('DROP TABLE motorcycle');
         $this->addSql('DROP TABLE supplier');
